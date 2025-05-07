d1b3af7caa9955f3afcfe12f9aeb9be03e6d2d4a
mythfrost <128764891+mythfrost@users.noreply.github.com>
2025-05-07 16:25:55 -0700

    Speed Maintained
    Speed is maintained through orbit based on how good your approach angle is

--- Scripts/spaceship.gd
+++ Scripts/spaceship.gd
@@ -10,16 +10,16 @@ var _orbit_angle: float   = 0.0
 
 @export var max_fuel := 100.0
 var current_fuel := max_fuel
-@export var thrust_power := 1500.0
-@export var fuel_burn_rate := 6.0
+@export var thrust_power := 1000.0
+@export var fuel_burn_rate := 20.0
 
 @export var max_turn_fuel       := 100.0
-@export var turn_fuel_burn_rate := 8.0
-@export var turn_speed          := 4.5
+@export var turn_fuel_burn_rate := 10.0
+@export var turn_speed          := 3.0
 var turn_fuel := max_turn_fuel
 
 @export var zoom_normal := Vector2(.8, .8)
-@export var zoomed_in   := Vector2(0.35, 0.35)
+@export var zoomed_in   := Vector2(0.35, .35)
 var is_zoomed := false
 
 @onready var cam: Camera2D = $Camera2D
@@ -34,11 +34,14 @@ func launch() -> void:
 func _physics_process(delta: float) -> void:
 	if is_launched:
 		if turn_fuel > 0:
+			var did_turn := false
 			if Input.is_action_pressed("turn_left"):
 				rotation -= turn_speed * delta
-				turn_fuel = max(turn_fuel - turn_fuel_burn_rate * delta, 0)
+				did_turn = true
 			elif Input.is_action_pressed("turn_right"):
 				rotation += turn_speed * delta
+				did_turn = true
+			if did_turn:
 				turn_fuel = max(turn_fuel - turn_fuel_burn_rate * delta, 0)
 		if Input.is_action_pressed("ui_accept") and current_fuel > 0:
 			current_fuel = max(current_fuel - fuel_burn_rate * delta, 0)
@@ -55,16 +58,14 @@ func _physics_process(delta: float) -> void:
 			return
 		
 func start_orbit(center: Vector2, radius: float, speed: float) -> void:
-	is_orbiting   = true
-	orbit_center  = center
-	_orbit_radius = radius
-
-	var radial = global_position - center
-	var cross_z = radial.x * velocity.y - radial.y * velocity.x
-	var dir_sign = 1 if cross_z >= 0 else -1
-	_orbit_speed = speed * dir_sign
-	_orbit_angle = radial.angle()
-
+	is_orbiting    = true
+	orbit_center   = center
+	_orbit_radius  = radius
+	var radial     = global_position - center
+	var tangent_dir = Vector2(-radial.y, radial.x).normalized()
+	var tang_speed = velocity.dot(tangent_dir)
+	_orbit_speed   = 1.2 * tang_speed / radius
+	_orbit_angle   = radial.angle()
 
 func stop_orbit() -> void:
 	is_orbiting = false
