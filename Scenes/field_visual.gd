53e294223a2544500100b2fe540eb96beb942cc3
mythfrost <128764891+mythfrost@users.noreply.github.com>
2025-06-09 19:06:05 -0700

    Adding final comments

--- Scenes/field_visual.gd
+++ Scenes/field_visual.gd
@@ -1,10 +1,13 @@
 extends ColorRect
 
+#Reference to the shader material
 var mat: ShaderMaterial
 
+#Called when the node is added to the scene
 func _ready():
 	mat = material as ShaderMaterial
 
+#Called every frame
 func _process(delta):
 	var t = mat.get_shader_parameter("shader_time") + delta
 	mat.set_shader_parameter("shader_time", t)
--- Scenes/thrust.gd
+++ Scenes/thrust.gd
@@ -1,6 +1,7 @@
 extends Control
 class_name Flame
 
+# Defines the points that shape the flame polygon
 @export var flame_points := PackedVector2Array([
 	Vector2(-10,  0),
 	Vector2( 10,  0),
@@ -8,12 +9,15 @@ class_name Flame
 	Vector2(-5, 40),
 ])
 
+# Called when the node is added to the scene
 func _ready() -> void:
 	set_process(true)
 	queue_redraw()
 
+# Called every frame
 func _process(delta: float) -> void:
 	queue_redraw()
 
+# Called when the control is drawn
 func _draw() -> void:
 	draw_colored_polygon(flame_points, Color.DARK_RED)
